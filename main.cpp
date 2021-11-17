aa0c38ed8ea8134af9ce1b715fae8d6098058875
LuizEduardoCardozo <eduard.cardoz@gmail.com>
2021-11-16 21:05:20 -0300

    feat: add memory structure

--- main.cpp
+++ main.cpp
@@ -1,9 +1,30 @@
+#include <stdint.h>
+#include <stdlib.h>
+
+
 #define EXIT_SUCCESS 0
+
+using Byte = unsigned char;
+using Word = unsigned short;
+
+struct Mem 
+{
+    static constexpr uint32_t MEM_SIZE = 1024 * 64;
+    Byte Data[MEM_SIZE];
+
+    void Init()
+    {   
+        /* Cleans all the memory */
+        for(size_t i=0; i<MEM_SIZE; i++) {
+            Data[i] = 0x00;
+        }
+
+    }
+
+};
 
 struct CPU
 {
-    using Byte = unsigned char;
-    using Word = unsigned short;
     
     Word PC;    // program counter
     Word SP;    // stack pointer;
@@ -26,7 +47,7 @@ struct CPU
         SP = 0x0100;                                            
         D = 0;                                                  
         C = Z = I = D = B = V = N = 0x0;                        
-        A = X = Y = 0x0;                                   
+        A = X = Y = 0x0;
     }   
 
 };
